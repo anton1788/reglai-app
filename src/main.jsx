fc49398afcec99445f6c47448ebffca23c965018
anton1788 <aksyanov.2014@yandex.ru>
2026-04-20 13:41:30 +0300

    3.0.0 откат

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 import { initOfflineModule, checkOfflineSupport } from './utils/offlineStorage'
 
 // ✅ Версия приложения
-const APP_VERSION = '4.0.0';
+const APP_VERSION = '5.0.0';
 
 // ─────────────────────────────────────────────────────────
 // 🔹 Регистрация Service Worker
