c2af0297b2e9712b3accb68acaf1a5d8ec0a4bcb
anton1788 <aksyanov.2014@yandex.ru>
2026-06-08 17:32:01 +0300

    9.9.30

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import UpdateModal from './components/UpdateModal.jsx'
 import { initOfflineModule, checkOfflineSupport } from './utils/offlineStorage'
 
 // ✅ Версия приложения
-const APP_VERSION = '9.9.29';
+const APP_VERSION = '9.9.30';
 
 // ─────────────────────────────────────────────────────────
 // 🔹 Глобальные переменные для модального окна обновлений
