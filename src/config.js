acf43381a35a36bf21147454411a581b6ef7cc0f
anton1788 <aksyanov.2014@yandex.ru>
2026-06-11 14:22:57 +0300

    3.11.2-beta

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // ✅ Единая точка управления версией приложения
-export const APP_VERSION = '3.10.2-beta';
+export const APP_VERSION = '3.11.2-beta';
 
 export const APP_CONFIG = {
   version: APP_VERSION,
