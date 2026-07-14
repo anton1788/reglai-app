f248a5f4dc3df32112611fedfc60f8ee414ad175
anton1788 <aksyanov.2014@yandex.ru>
2026-07-14 16:31:51 +0300

    0.0.5-beta

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import UpdateModal from './components/UpdateModal.jsx'
 import { initOfflineModule, checkOfflineSupport } from './utils/offlineStorage'
 
 // ✅ Версия приложения
-const APP_VERSION = '0.0.4-beta';
+const APP_VERSION = '0.0.5-beta';
 
 // ─────────────────────────────────────────────────────────
 // 🔹 Глобальные переменные для модального окна обновлений
